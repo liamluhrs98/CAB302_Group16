065eea2c6d2f60721526521fc62194d2739acef4
liamluhrs98 <liam.luhrs@connect.qut.edu.au>
2018-05-20 13:39:47 +1000

    item object

--- src/stock/item.java
+++ src/stock/item.java
@@ -1,5 +1,25 @@
 package stock;
 
 public class item {
-	//added branches
+	
+	//Set up object
+	private String name; 		//Item name
+	private int quantity;		//Quantity of item
+	private int manu_cost;		//Manufacturing cost of item
+	private int ret_price;		//Retail price of item
+	private int reord_point;	//Reordering point of item
+	private int reord_amt;		//Quantity to reorder of item, once reorder point is reached
+	private int temp;			//Temp to store item at
+	
+	//Construct item object with variables
+	public item(String name, int quantity, int manu_cost, int ret_price, int reord_point, int reord_amt, int temp) {
+		this.name = name;
+		this.quantity = quantity;
+		this.manu_cost = manu_cost;
+		this.ret_price = ret_price;
+		this.reord_point = reord_point;
+		this.reord_amt = reord_amt;
+		this.temp = temp;
+	}
+	
 }
--- src/stock/stockTests.java
+++ src/stock/stockTests.java
@@ -1,18 +1,31 @@
 package stock;
 
-//import static org.junit.Assert*;
+//Import
+import static org.junit.Assert.*;
+import java.util.Random;
+import org.junit.Test;
 
 public class stockTests {
 
 	//Set up test data
 	private static Random random = new Random();
-	private static String[] item_names = new String[] {
-			"Rice",
-			"Rice",
-			"Rice",
-			"Rice",
-			"Rice",
-			"Rice",
+	private static String[] test_item_prop = new String[] {
+			"rice",
+			"beans",
+			"pasta",
+			"biscuits",
+			"nuts",
+			"chips",
+			"chocolate",
+			"bread",
+			"mushroom",
+			"tomatoes",
+			"lettuce",
+			"grapes",
+			"asparagus",
+			"celery",
+			"chicken",
 	};
+
 	
 }
