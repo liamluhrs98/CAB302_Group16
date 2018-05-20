59bd343ea3f763d1389866c1d40f2ef390831680
harry <harry@DESKTOP-FJFTQ88.gateway>
2018-05-20 18:57:45 +1000

    further object work

--- src/delivery/refrigeratedTruck.java
+++ src/delivery/refrigeratedTruck.java
@@ -4,8 +4,9 @@ public class refrigeratedTruck extends truck {
 
 	private String item;
 	private int quantity;
+	private int temp;
 	
-	public refrigeratedTruck(String item, int quantity) {
+	public refrigeratedTruck(String item, int quantity, int temp) {
 		// TODO Auto-generated constructor stub
 		super("refrigerated");
 	}
