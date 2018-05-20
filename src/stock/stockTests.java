992866de1a218e1ded7647645d88a0bd29874598
liamluhrs98 <liam.luhrs@connect.qut.edu.au>
2018-05-20 12:30:49 +1000

    added testing data

--- src/stock/stockTests.java
+++ src/stock/stockTests.java
@@ -1,5 +1,18 @@
 package stock;
 
+//import static org.junit.Assert*;
+
 public class stockTests {
 
+	//Set up test data
+	private static Random random = new Random();
+	private static String[] item_names = new String[] {
+			"Rice",
+			"Rice",
+			"Rice",
+			"Rice",
+			"Rice",
+			"Rice",
+	};
+	
 }
