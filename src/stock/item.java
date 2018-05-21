1e7494d854517a56e6a9b91667cd82b11a9a85fa
liamluhrs98 <liam.luhrs@connect.qut.edu.au>
2018-05-21 11:14:13 +1000

    complete items object

--- src/stock/item.java
+++ src/stock/item.java
@@ -4,17 +4,15 @@ public class item {
 	
 	//Set up object
 	private String name; 		//Item name
-	private int quantity;		//Quantity of item
 	private int manu_cost;		//Manufacturing cost of item
 	private int ret_price;		//Retail price of item
 	private int reord_point;	//Reordering point of item
 	private int reord_amt;		//Quantity to reorder of item, once reorder point is reached
-	private int temp;			//Temp to store item at
+	private int temp;			//Temperature to store item at
 	
 	//Construct item object with variables
-	public item(String name, int quantity, int manu_cost, int ret_price, int reord_point, int reord_amt, int temp) {
+	public item(String name, int manu_cost, int ret_price, int reord_point, int reord_amt, int temp) {
 		this.name = name;
-		this.quantity = quantity;
 		this.manu_cost = manu_cost;
 		this.ret_price = ret_price;
 		this.reord_point = reord_point;
@@ -25,6 +23,26 @@ public class item {
 	
 	public String getItemName() {
 		return this.name;
+	}
+	
+	public int getItemManuCost() {
+		return this.manu_cost;
+	}
+	
+	public int getItemRetPrice() {
+		return this.ret_price;
+	}
+	
+	public int getItemReordPoint() {
+		return this.reord_point;
+	}
+	
+	public int getItemReordAmt() {
+		return this.reord_amt;
+	}
+	
+	public int getItemTemp() {
+		return this.temp;
 	}
 	
 	
--- src/stock/testMain.java
+++ src/stock/testMain.java
@@ -4,7 +4,7 @@ public class testMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		stock testStock = new stock();
+		
 	}
 
 }
