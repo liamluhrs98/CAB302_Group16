3c78c2b95eed931924bc52c7984d36d5e962f1f8
liamluhrs98 <liam.luhrs@connect.qut.edu.au>
2018-05-21 12:27:32 +1000

    import item prop function half done

--- src/stock/stockMain.java
+++ src/stock/stockMain.java
@@ -1,10 +1,31 @@
 package stock;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class stockMain {
 	
 	public void ImportItemProp() {
-		//
-	}
+		String csvfile = "C:/Users/liaml/Desktop/itemproperties.csv";
+        String line = "";
+        String csvSplitBy = ",";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvfile))) {
+
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] entry = line.split(csvSplitBy);
+
+                System.out.println(entry[0]+ "=" + entry[1]);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 	
 	public void CurrentCapital() {
 		//return capital
