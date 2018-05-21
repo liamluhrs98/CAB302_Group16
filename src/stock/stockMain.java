6c640493b8504a4b519be9952b5e47ab32be6849
liamluhrs98 <liam.luhrs@connect.qut.edu.au>
2018-05-21 16:28:18 +1000

    import item properties function half completed

--- src/stock/stockMain.java
+++ src/stock/stockMain.java
@@ -7,17 +7,57 @@ import java.io.IOException;
 public class stockMain {
 	
 	public void ImportItemProp() {
-		String csvfile = "C:/Users/liaml/Desktop/itemproperties.csv";
+		String itemprop_location = "C:/Users/liaml/Desktop/302 Files/itemproperties.csv";
         String line = "";
         String csvSplitBy = ",";
 
-        try (BufferedReader br = new BufferedReader(new FileReader(csvfile))) {
+        
+        //Initialise temporary variables to use for creating items
+        String adhoc_name = "";			//Item name
+                
+        try (BufferedReader br = new BufferedReader(new FileReader(itemprop_location))) {
 
             while ((line = br.readLine()) != null) {
 
-                // use comma as separator
+                //Split on comma (as its a csv)
                 String[] entry = line.split(csvSplitBy);
-
+                
+                //Put the name into the adhoc variable
+                adhoc_name = entry[0];
+                
+                //Convert remaining strings to int
+                try (int adhoc_manu_cost = Integer.parseInt(entry[1])) {
+            	   
+                } catch (NumberFormatException e) {
+                	System.out.println("Cant convert " + entry[1] + " to type int.");
+                }
+                
+                try (int adhoc_ret_price = Integer.parseInt(entry[2])) {
+             	   
+                } catch (NumberFormatException e) {
+                	System.out.println("Cant convert " + entry[2] + " to type int.");
+                }
+                try (int adhoc_reord_point = Integer.parseInt(entry[3])) {
+             	   
+                } catch (NumberFormatException e) {
+                	System.out.println("Cant convert " + entry[3] + " to type int.");
+                }
+                try (int adhoc_reord_amt = Integer.parseInt(entry[4])) {
+             	   
+                } catch (NumberFormatException e) {
+                	System.out.println("Cant convert " + entry[4] + " to type int.");
+                }
+                try (int adhoc_temp = Integer.parseInt(entry[5])) {
+             	   
+                } catch (NumberFormatException e) {
+                	System.out.println("Cant convert " + entry[5] + " to type int.");
+                }
+                
+                            
+                
+                
+                
+                
                 System.out.println(entry[0]+ "=" + entry[1]);
 
             }
