9d10adbc3891bb78ea2063045c44d79d48da2c88
harry <harry@172.19.54.96>
2018-05-23 12:15:01 +1000

    methods work

--- src/delivery/deliveryMain.java
+++ src/delivery/deliveryMain.java
@@ -3,12 +3,18 @@ package delivery;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class deliveryMain {
 	
 	public static void main(String[] args) {
 		//Import CSV
 		//Sort into object/array/hashmap
+		int[] arr = null;
+		String[] names = new String[30];
+		ArrayList<order> orderlist = new ArrayList<order>();
 		String csvfile = "C:/Users/harry/Desktop/sales_log_0.csv";
 		String line = "";
 		String csvSplitBy = ",";
@@ -19,14 +25,40 @@ public class deliveryMain {
 
                 // use comma as separator
                 String[] name = line.split(csvSplitBy);
-
-                System.out.println(name[0]+ "=" + name[1]);
-
+                String[] amount = new String[] {name[1]};
+                names = new String[] {name[0]};
+                
+                for (int i = 0; i < amount.length; i++) {
+        			arr = strArrayToIntArray(amount);
+        		}
+                
+                
+                for (int i = 0; i < amount.length; i++) {
+                	orderlist.add(new order(names[i], arr[i]));
+                }
+                
+                
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+		
+		for (int i = 0; i< names.length; i++) {
+			String[][] test = new String[50][50];
+			test[0][i] = names[i];
+			test[1][i] = Integer.toString(arr[i]);
+			System.out.println(test[0][i] + "=" + test[1][i]);
+		}
+	}
+	
+	public static int[] strArrayToIntArray(String[] a){
+	    int[] b = new int[a.length];
+	    for (int i = 0; i < a.length; i++) {
+	        b[i] = Integer.parseInt(a[i]);
+	    }
+
+	    return b;
 	}
 	
 	public void sortFood() {
