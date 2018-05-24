a6cb86b51715f745e5922867cf7ba58c4b9573fe
liamluhrs98 <liam.luhrs@connect.qut.edu.au>
2018-05-25 09:44:32 +1000

    finished all import functions, started create item function

--- src/stock/stockMain.java
+++ src/stock/stockMain.java
@@ -3,72 +3,73 @@ package stock;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class stockMain {
 	
-	public static int[] s_strArrayToIntArray(String[] a) {
-		int[] b = new int[a.length];
-		for (int i = 0; i < b.length; i++) {
-			try {
-				b[i] = Integer.parseInt(a[i]); 
-						
-			} catch (NumberFormatException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return b;
-	}
+    private static ArrayList<String> names = new ArrayList<String>();
+    private static ArrayList<Integer> manu_cost = new ArrayList<Integer>();
+    private static ArrayList<Integer> ret_price = new ArrayList<Integer>();
+    private static ArrayList<Integer> reord_point = new ArrayList<Integer>();
+    private static ArrayList<Integer> reord_amt = new ArrayList<Integer>();
+    private static ArrayList<String> temp = new ArrayList<String>();
+    
+    
+    //String ArrayList to Integer ArrayList Method
+    public static int s_StrToInt(String a){
+        //ArrayList<Integer> b = new ArrayList<Integer>();
+        int b = 0;
+    	try {
+            b = Integer.parseInt(a);
+        } catch(NumberFormatException nfe) {
+         	System.out.println("Could not parse " + a + " as an integer, please check CSV.");
+         	nfe.printStackTrace();
+        }
+        return b;
+    }
 	
-	public void ImportItemProp() {
-		String itemprop_location = "C:/Users/liaml/Desktop/302 Files/itemproperties.csv";
+    public static void ImportItemProp() {
+		String itemprop_location = "C:/Users/liaml/Desktop/302 Files/item_properties.csv";
         String line = "";
         String csvSplitBy = ",";
-        
-        int count = 0;
-        
-        try (BufferedReader br = new BufferedReader(new FileReader(itemprop_location))) {
 
-            while ((line = br.readLine()) != null) {
-            	count = count + 1;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        String[] names = new String[count];
-        String[] manu_cost = new String[count];
-        String[] ret_price = new String[count];
-        String[] reord_point = new String[count];
-        String[] reord_amt = new String[count];
-        String[] temp = new String[count];
-                
-        int count2 = 0;
-        
         try (BufferedReader br = new BufferedReader(new FileReader(itemprop_location))) {
 
             while ((line = br.readLine()) != null) {
 
                 //Split on comma (as its a csv)
                 String[] entry = line.split(csvSplitBy);
+                 
+                names.add(entry[0]);
+                manu_cost.add(s_StrToInt(entry[1]));
+                ret_price.add(s_StrToInt(entry[2]));
+                reord_point.add(s_StrToInt(entry[3]));
+                reord_amt.add(s_StrToInt(entry[4]));
                 
-                count2 = count2 + 1;
-                
-                names[count2] = entry[0];
-                manu_cost[count2] = entry[1];
-                ret_price[count2] = entry[2];
-                reord_point[count2] = entry[3];
-                reord_amt[count2] = entry[4];
-                temp[count2] = entry[5];
+                //leaving temp as string because some items have no temp
+                try {
+                	temp.add(entry[5]);
+                }
+                catch(ArrayIndexOutOfBoundsException oob) {
+                	temp.add(" ");
+                }
                 
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException ioe) {
+        	System.out.println("IOException");
+            ioe.printStackTrace();
         }
     }
 	
-	public void CurrentCapital() {
-		//return capital
+	public void CreateItems() {
+		for (int i = 0; i < names.size(); i++) {
+			
+		}
+	}
+	
+	
+	public void UpdateCapital() {
+	
 	}
 	
 	public void CurrentStock() {
@@ -86,4 +87,13 @@ public class stockMain {
 	public void ExportOrder() {
 		
 	}
+	
+	public static void main(String[] args) {
+		ImportItemProp();
+		for (int i = 0; i < names.size(); i++) {
+			System.out.println(names.get(i) + " : " + manu_cost.get(i) + " : " + ret_price.get(i) + " : " + reord_point.get(i) + " : " + reord_amt.get(i) + " : " + temp.get(i));
+		}
+	}
+
+
 }
